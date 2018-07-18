478ad6a54e8706d1fd1bcb6782d4bd0a1715c872
xiaohe <510001913@qq.com>
2018-07-18 21:14:31 +0800

    heros

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero';
+
+export const  HEROES: Hero[] = [
+  {id: 1, name: '灰烬之灵'},
+  {id: 2, name: '大地之灵'},
+  {id: 3, name: '熊猫酒仙'},
+  {id: 4, name: '龙骑士'},
+  {id: 5, name: '双头龙'},
+  {id: 6, name: '尙钰焜'}
+];
