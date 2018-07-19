70bff7bada18791d2dca2598771c43ae521f9805
xiaohe <510001913@qq.com>
2018-07-19 17:13:55 +0800

    英雄2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import {MessageService} from './message.service';
+import {HeroService} from './hero.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,7 @@ import { MessagesComponent } from './messages/messages.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  providers: [MessageService]
+  providers: [HeroService],
 })
 export class HeroesComponent implements OnInit {
 
